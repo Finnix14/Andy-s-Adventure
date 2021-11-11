7737929aab3e0e1a0638639be69ddae2527042e5
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2021-11-11 16:04:56 +0000

    Last Push of Thursday at college

--- GitHubFMP/Assets/Scripts/MainMenu.cs
+++ GitHubFMP/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Game");
     }
+
+
     public void SettingsMenu()
     {
         SceneManager.LoadScene("SettingsMenu");
