36b7a8bde0c20dba504d3d94e417bf7325321695
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2021-11-12 20:02:51 +0000

    12/11/21 push

--- GitHubFMP/Assets/Scripts/PauseMenu.cs
+++ GitHubFMP/Assets/Scripts/PauseMenu.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
-    public static bool isPaused;
+    public static bool isPaused = false;
+    public Canvas canvas;
 
-
-    private void Start()
+    void Start()
     {
         pauseMenu.SetActive(false);
     }
 
-    private void Update()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (isPaused)
             {
@@ -27,6 +27,12 @@ public class PauseMenu : MonoBehaviour
             }
         }
     }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
 
     public void PauseGame()
     {
@@ -34,11 +40,13 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         isPaused = true;
     }
-
-    public void ResumeGame()
+    public void GoToMainMenu()
     {
-        pauseMenu.SetActive(false);
         Time.timeScale = 1f;
-        isPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
