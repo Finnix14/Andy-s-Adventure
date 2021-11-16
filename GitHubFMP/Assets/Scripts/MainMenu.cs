16f6ad7145437607f461337431c0a774d208708e
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2021-11-16 14:05:09 +0000

    new working game

--- GitHubFMP/Assets/Scripts/MainMenu.cs
+++ GitHubFMP/Assets/Scripts/MainMenu.cs
@@ -25,7 +25,7 @@ public class MainMenu : MonoBehaviour
     }
     public void LevelTwo()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Level2");
     }
 
     public void LevelThree()
