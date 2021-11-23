d6f48e9be38d2b60566aaed31c6ccc8b9708362f
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2021-11-23 13:22:22 +0000

    still no working enemyai bird

--- GitHubFMP/Assets/Scripts/MainMenu.cs
+++ GitHubFMP/Assets/Scripts/MainMenu.cs
@@ -33,5 +33,9 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("LevelThree");
     }
 
+    public void FinalLevel()
+    {
+        SceneManager.LoadScene("FinalLevel");
+    }
     
 }
