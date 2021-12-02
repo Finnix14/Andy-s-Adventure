f1eaa7ab35c0875146d30fc04766b9979a3adff1
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2021-12-02 14:32:17 +0000

    Thursday Push. Some Bug Fixes fixed

---
+++ GitHubFMP/Assets/Scripts/MenuLoad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoad : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(ToMenu());
+    }
+
+    IEnumerator ToMenu()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(1);
+    }
+
+}
+
+
+
